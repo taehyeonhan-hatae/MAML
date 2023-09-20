0b02da36c65b39394baedc084c449f19fd1e864a
임종민 <skku.jmlim@gmail.com>
2023-09-20 10:36:30 +0900

    ArcFace 실험

--- meta_neural_network_architectures_OLE.py
+++ meta_neural_network_architectures_OLE.py
@@ -933,12 +933,18 @@ class VGGReLUNormNetwork(nn.Module):
         self.encoder_features_shape = list(out.shape)
         out = out.view(out.shape[0], -1)
 
-        self.layer_dict['linear'] = MetaLinearLayer(input_shape=(out.shape[0], np.prod(out.shape[1:])),
-                                                    num_filters=512, use_bias=True)
+        if self.args.loss_function == "ArcFace":
+            self.layer_dict['linear'] = MetaLinearLayer(input_shape=(out.shape[0], np.prod(out.shape[1:])),
+                                                        num_filters=512, use_bias=True)
 
-        out = self.layer_dict['linear'](out)
+            out = self.layer_dict['linear'](out)
 
-        self.layer_dict['head'] = ArcFace(in_features=out.shape[1], out_features=self.args.num_classes_per_set).to(device=self.device)
+            self.layer_dict['head'] = ArcFace(in_features=out.shape[1], out_features=self.args.num_classes_per_set).to(device=self.device)
+
+        elif self.args.loss_function == "Softmax":
+            self.layer_dict['linear'] = MetaLinearLayer(input_shape=(out.shape[0], np.prod(out.shape[1:])),
+                                                        num_filters=self.num_output_classes, use_bias=True)
+            out = self.layer_dict['linear'](out)
 
 
     def forward(self, x, num_step, label, params=None, training=False, backup_running_statistics=False, isDropout=False):
@@ -986,8 +992,17 @@ class VGGReLUNormNetwork(nn.Module):
 
         embedding = out
 
-        out = self.layer_dict['linear'](out, param_dict['linear'])
-        out, original_logits = self.layer_dict['head'](out, label, param_dict['head'])
+        if self.args.loss_function == "ArcFace":
+
+            out = self.layer_dict['linear'](out, param_dict['linear'])
+            out, original_logits = self.layer_dict['head'](out, label, param_dict['head'])
+
+        elif self.args.loss_function == "Softmax":
+            out = self.layer_dict['linear'](out, param_dict['linear'])
+            original_logits = out
+        else:
+            original_logits = "no seleted loss function"
+            print(original_logits)
 
         return out, original_logits
 
