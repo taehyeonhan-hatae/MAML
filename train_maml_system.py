eb4c7b590025af58c8ce0d49b354a3df151f40a5
임종민 <skku.jmlim@gmail.com>
2023-10-14 05:00:16 +0900

    Gradient rate 실험결과

--- train_maml_system.py
+++ train_maml_system.py
@@ -16,7 +16,6 @@ from multiprocessing import freeze_support
 ## python train_maml_system.py --name_of_args_json_file experiment_config/my_experiment/MAML+Arbiter+CurricularFace.json --gpu_to_use 0
 ## python train_maml_system.py --name_of_args_json_file experiment_config/my_experiment/MAML+OLE.json --gpu_to_use 0
 ## python train_maml_system.py --name_of_args_json_file experiment_config/my_experiment/MAML+OLE+Arbiter.json --gpu_to_use 0
-## python train_maml_system.py --name_of_args_json_file experiment_config/my_experiment/MAML+Arbiter_5way_1shot.json --gpu_to_use 0
 ## python train_maml_system.py --name_of_args_json_file experiment_config/my_experiment/MAML+Arbiter_5way_5shot.json --gpu_to_use 0
 
 if __name__ == '__main__':
