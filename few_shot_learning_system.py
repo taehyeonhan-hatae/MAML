d1ced193b16e43134750a6d974e805a197b04ea9
SKKUJMLim <skku.jmlim@gmail.com>
2023-10-25 08:59:27 +0900

    Domain Adaptation

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -71,6 +71,7 @@ class MAMLFewShotClassifier(nn.Module):
                 nn.Linear(input_dim, input_dim),
                 nn.ReLU(inplace=True),
                 nn.Linear(input_dim, output_dim),
+                nn.Sigmoid()
                 #nn.Softplus(beta=2)
             ).to(device=self.device)
 
