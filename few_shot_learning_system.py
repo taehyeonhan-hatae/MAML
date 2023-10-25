365f6766cd656069db485b15889787ef504f08bd
임종민 <skku.jmlim@gmail.com>
2023-10-26 05:49:24 +0900

    Domain Adaptation

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -267,13 +267,12 @@ class MAMLFewShotClassifier(nn.Module):
                     #     per_step_task_embedding.append(v.mean())
 
                     for i in range(len(support_loss_grad)):
-                        per_step_task_embedding.append(support_loss_grad[i].mean())
-                        #per_step_task_embedding.append(support_loss_grad[i].norm())
+                        # per_step_task_embedding.append(support_loss_grad[i].mean())
+                        per_step_task_embedding.append(support_loss_grad[i].clone().detach().mean())
 
                     for i in range(len(support_loss_grad)):
-                        # per_step_task_embedding.append(support_loss_grad[i].mean())
+                        # per_step_task_embedding.append(support_loss_grad[i].norm()) # 반드시 detach 해줘야한다. 평균도?
                         per_step_task_embedding.append(support_loss_grad[i].clone().detach().norm())
-                        # per_step_task_embedding.append(support_loss_grad[i].norm())
 
                     per_step_task_embedding = torch.stack(per_step_task_embedding)
 
