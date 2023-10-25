b7d0a264f1446693a909571ecc52a43a61b4f82c
임종민 <skku.jmlim@gmail.com>
2023-10-25 20:26:23 +0900

    Domain Adaptation

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -73,7 +73,8 @@ class MAMLFewShotClassifier(nn.Module):
                 nn.Linear(input_dim, output_dim),
                 ## nn.Sigmoid()
                 ## nn.Tanh()
-                nn.Softplus(beta=2)
+                ## nn.Softplus(beta=2) # GAP
+                nn.Softplus() # CxGrad
             ).to(device=self.device)
 
         self.inner_loop_optimizer.initialise(
