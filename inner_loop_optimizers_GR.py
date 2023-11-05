c7644054bda89ab997e4315d366eb1af874b4c1e
SKKUJMLim <skku.jmlim@gmail.com>
2023-11-05 16:47:29 +0900

    Domain Adaptation

--- inner_loop_optimizers_GR.py
+++ inner_loop_optimizers_GR.py
@@ -139,7 +139,7 @@ class LSLRGradientDescentLearningRule(nn.Module):
                                                   self.names_learning_rates_dict[key.replace(".", "-")][num_step] * generated_alpha_params[key] * names_grads_wrt_params_dict[key]
 
                 if self.args.SWA:
-                    alpha = 1.0 / num_step + 1
+                    alpha = 1.0 / (num_step + 1)
                     names_weights_dict[key] = names_weights_dict[key] * alpha
                     updated_names_weights_dict[key] = (updated_names_weights_dict[key] + names_weights_dict[key]) / 2
 
