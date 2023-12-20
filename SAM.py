74837f0115c481c2f6aa7a24116460e6e9f11788
임종민 <skku.jmlim@gmail.com>
2023-12-20 17:45:44 +0900

    Sharpness-Aware Minimization

--- SAM.py
+++ SAM.py
@@ -2,17 +2,15 @@ import torch
 
 
 class SAM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, rho_scheduler, rho=0.0005, adaptive=False, perturb_eps=1e-12, **kwargs):
-
-        defaults = dict(adaptive=adaptive, **kwargs)
+    def __init__(self, params, base_optimizer, rho=0.0005, adaptive=True, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+        # rho=0.05
+        defaults = dict(rho=rho, adaptive=adaptive, **kwargs)
         super(SAM, self).__init__(params, defaults)
 
-        self.base_optimizer = base_optimizer#(self.param_groups, **kwargs)
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
         self.param_groups = self.base_optimizer.param_groups
-        self.rho_scheduler = rho_scheduler
-        self.rho_t = rho
-        self.adaptive = adaptive
-        self.perturb_eps = perturb_eps
+        self.defaults.update(self.base_optimizer.defaults)
 
     @torch.no_grad()
     def first_step(self, zero_grad=False):
@@ -20,7 +18,7 @@ class SAM(torch.optim.Optimizer):
         grad_norm = self._grad_norm()
         for group in self.param_groups:
 
-            scale = self.rho_t / (grad_norm + self.perturb_eps)
+            scale = group["rho"] / (grad_norm + 1e-12)
 
             for p in group["params"]:
                 if p.grad is None: continue
@@ -29,29 +27,20 @@ class SAM(torch.optim.Optimizer):
                 # 여기서 w에 대한 gradient를 저장해두어야한다
                 self.state[p]["old_p_grad"] = p.grad.clone()
 
-                e_w = p.grad * scale.to(p)
-                if self.adaptive:
-                    e_w *= torch.pow(p, 2)
-
+                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
                 p.add_(e_w)  # climb to the local maximum "w + e(w)"
 
         if zero_grad: self.zero_grad()
 
     @torch.no_grad()
-    def second_step(self, balance, zero_grad=False):
+    def second_step(self, zero_grad=False, balance=0.7):
 
         for group in self.param_groups:
             for p in group["params"]:
                 if p.grad is None: continue
-
-                ## self.state[p]['old_p_grad']가 w에 대한 gradient이고
-                ## p.grad는 w + e(w)에서의 gradient이다 이다
-
                 p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
 
-                # 어느게 맞을까?
-                p.grad = (1 - balance) * self.state[p]["old_p_grad"] + balance * p.grad
-                # p.grad = balance * self.state[p]["old_p_grad"] + (1 - balance) * p.grad
+                p.grad = torch.tensor(1 - balance) * self.state[p]["old_p_grad"] + torch.tensor(balance) * p.grad
 
         self.base_optimizer.step()  # do the actual "sharpness-aware" update
 
@@ -70,7 +59,7 @@ class SAM(torch.optim.Optimizer):
         shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
         norm = torch.norm(
                     torch.stack([
-                        ((torch.abs(p) if self.adaptive else 1.0) * p.grad).norm(p=2).to(shared_device)
+                        ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(shared_device)
                         for group in self.param_groups for p in group["params"]
                         if p.grad is not None
                     ]),
