50778119fea30461dd4b91546566d21d5f663086
SKKUJMLim <skku.jmlim@gmail.com>
2024-01-27 06:46:11 +0900

    Without SAM

--- few_shot_learning_system_WO_SAM.py
+++ few_shot_learning_system_WO_SAM.py
@@ -476,8 +476,25 @@ class MAMLFewShotClassifier(nn.Module):
         preds = self.classifier.forward(x=x, params=weights,
                                         training=training,
                                         backup_running_statistics=backup_running_statistics, num_step=num_step)
+        if training_phase:
+            if self.args.smoothing:
+                criterion = LabelSmoothingCrossEntropy(smoothing=0.1)
+                loss = criterion(preds, y)
+            elif self.args.knowledge_distillation:
+                if not soft_target == None:
+                    # alpha = 0.1
+                    alpha = epoch / self.args.total_epochs
+                    loss = knowledge_distillation_loss(student_logit=preds, teacher_logit=soft_target, labels=y,
+                                                       label_loss_weight=(1.0 - alpha), soft_label_loss_weight=alpha,
+                                                       Temperature=1.0)
+                else:
+                    loss = F.cross_entropy(input=preds, target=y)
+            else:
+                loss = F.cross_entropy(input=preds, target=y)
+        else:
+            loss = F.cross_entropy(input=preds, target=y)
 
-        loss = F.cross_entropy(input=preds, target=y)
+        return loss, preds
 
         return loss, preds
 
