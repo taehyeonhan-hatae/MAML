50afdbd609bdbfaceb4af699012181c23b354c4e
임종민 <skku.jmlim@gmail.com>
2024-01-26 11:40:43 +0900

    Knowledge Distillation

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -452,8 +452,8 @@ class MAMLFewShotClassifier(nn.Module):
                 loss = criterion(preds, y)
             elif self.args.knowledge_distillation:
                 if not soft_target == None:
-                    alpha = 0.1
-                    # alpha = epoch / self.args.total_epochs
+                    # alpha = 0.1
+                    alpha = epoch / self.args.total_epochs
                     loss = knowledge_distillation_loss(student_logit=preds, teacher_logit=soft_target, labels=y,
                                                        label_loss_weight=(1.0 - alpha), soft_label_loss_weight=alpha,
                                                        Temperature=1.0)
