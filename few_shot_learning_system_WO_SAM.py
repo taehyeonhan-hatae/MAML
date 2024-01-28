8ced21f95c069c63b23469d5769e99b969676b71
SKKUJMLim <skku.jmlim@gmail.com>
2024-01-28 16:36:35 +0900

    MTL-PCGrad

--- few_shot_learning_system_WO_SAM.py
+++ few_shot_learning_system_WO_SAM.py
@@ -486,7 +486,7 @@ class MAMLFewShotClassifier(nn.Module):
                     alpha = epoch / self.args.total_epochs
                     loss = knowledge_distillation_loss(student_logit=preds, teacher_logit=soft_target, labels=y,
                                                        label_loss_weight=(1.0 - alpha), soft_label_loss_weight=alpha,
-                                                       Temperature=2.0)
+                                                       Temperature=1.0)
                 else:
                     loss = F.cross_entropy(input=preds, target=y)
             else:
