69ae7c2290b63eda89c01dcfe6518494bd2ae047
임종민 <skku.jmlim@gmail.com>
2024-02-27 22:30:28 +0900

    experiment config

--- train_maml_system.py
+++ train_maml_system.py
@@ -22,6 +22,7 @@ from multiprocessing import freeze_support
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_5way_1shot.json --gpu_to_use 0
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_5way_5shot.json --gpu_to_use 0
 
+## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_5way_1shot_filter48.json --gpu_to_use
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_5way_5shot_filter48.json --gpu_to_use 0
 
 ## python train_maml_system.py --name_of_args_json_file experiment_config/MAML+Arbiter_5way_1shot_filter64.json --gpu_to_use 0
