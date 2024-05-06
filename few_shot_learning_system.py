f4cf0e68bf398400da080ef632e7b31fc9a47d08
SKKUJMLim <skku.jmlim@gmail.com>
2024-05-06 12:11:06 +0900

    arbiter에서 prompt 제거

--- few_shot_learning_system.py
+++ few_shot_learning_system.py
@@ -8,7 +8,6 @@ import torch.optim as optim
 
 from meta_neural_network_architectures import VGGReLUNormNetwork,ResNet12, Arbiter
 from inner_loop_optimizers_GR import GradientDescentLearningRule, LSLRGradientDescentLearningRule
-import prompters
 
 def set_torch_seed(seed):
     """
@@ -68,12 +67,6 @@ class MAMLFewShotClassifier(nn.Module):
                                                                     args=self.args,
                                                                     learning_rate=self.task_learning_rate)
 
-
-        # Prompter
-        if self.args.prompter:
-            # choices=['padding', 'random_patch', 'fixed_patch'],
-            method = 'padding'
-            self.prompter = prompters.__dict__[method](self.args).to(device)
 
         # Gradient Arbiter
         if self.args.arbiter:
@@ -262,9 +255,6 @@ class MAMLFewShotClassifier(nn.Module):
             x_target_set_task = x_target_set_task.view(-1, c, h, w)
             y_target_set_task = y_target_set_task.view(-1)
 
-            if self.args.prompter:
-                # prompted_images = self.prompter(x_support_set_task)
-                x_support_set_task = self.prompter(x_support_set_task)
 
             for num_step in range(num_steps):
                 support_loss, support_preds  = self.net_forward(
